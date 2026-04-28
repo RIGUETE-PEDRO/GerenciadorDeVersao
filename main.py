15b3cea39b5fc6f824f80e46080b8751f6fe9cbf
Pedro Henrique <riguetepedro0@gmail.com>
2026-04-28 15:09:28 -0300

    subindo interface grafica

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-if __init__ = '__main__'
+import sys
+from PySide6.QtWidgets import QApplication, QWidget, QLabel
+
+app = QApplication(sys.argv)
+
+janela = QWidget()
+janela.setWindowTitle("Minha primeira tela")
+janela.resize(400, 300)
+
+label = QLabel("Olá, esta é minha tela!", janela)
+label.move(100, 130)
+
+janela.show()
+
+sys.exit(app.exec())
